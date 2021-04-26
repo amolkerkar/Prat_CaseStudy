eb6dca2ef1cc465b4c230cf6db8b350954b4a326
prat7562 <81015406+prat7562@users.noreply.github.com>
2021-04-26 15:11:47 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,7 @@
-/**
- * @file main.c
- * @author Pratik Lodha
- * @brief Project for Heat Control of a seat
- * @version 0.1 (Act-1)
- * @date 2021-04-24
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-#include <avr\io.h>
-#include <util\delay.h>
-/**
- * @brief main function where code execution starts
- * 
- * @return int 
- */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
 int main(void)
 {
 port_def();
