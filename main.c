9344beebe293a6a3bf18cb759d159609674285a4
prat7562 <81015406+prat7562@users.noreply.github.com>
2021-04-26 15:15:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#include <avr\io.h>
-#include <util\delay.h>
+#include <avr/io.h>
+#include <util/delay.h>
 /**
  * @brief main function where code execution
  * 
