22b452ae2b402385223c94464abd3865317665f9
prat7562 <81015406+prat7562@users.noreply.github.com>
2021-04-27 17:09:22 +0530

    Update and rename PortsDefine.h to Act_1.h

---
+++ inc/Act_1.h
@@ -0,0 +1,6 @@
+#ifndef ACT_1_H_INCLUDED
+#define ACT_1_H_INCLUDED
+
+Act_1();
+
+#endif // ACT_1_H_INCLUDED
--- inc/PortsDefine.h
+++
@@ -1,9 +0,0 @@
-#ifndef PORTSDEFINE_H_INCLUDED
-#define PORTSDEFINE_H_INCLUDED
-/**
- * @brief Construct a new port def object
- * 
- */
-port_def();
-
-#endif // PORTSDEFINE_H_INCLUDED
