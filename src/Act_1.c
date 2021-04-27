4a512f9a1d3fb878a5156af90914c2c9db9fcb1a
prat7562 <81015406+prat7562@users.noreply.github.com>
2021-04-27 20:04:08 +0530

    Update Act_1.c

--- src/Act_1.c
+++ src/Act_1.c
@@ -15,8 +15,17 @@
  *
  * @return int
  */
-void Act_1()
+void Activity_1()
 {
+DDRB |= (1<<PB0); // set PB0=1; set bit for Led output
+DDRB |= (1<<PB1); // set PB1=1; set bit for heater output
+
+DDRD &=~(1<<PD2); // set PD2=0; clear bit for input
+PORTD |= (1<<PD2);
+
+DDRD &=~(1<<PD3); // set PD3=0; clear bit for input
+PORTD |= (1<<PD3);
+
     while(1)
     {
         if(!(PIND&(1<<PD2))&&!(PIND&(1<<PD3)))
