c03dc0f37b7c36e41a7eaa4a74737adcae2ac337
prat7562 <81015406+prat7562@users.noreply.github.com>
2021-04-30 01:09:08 +0530

    added doxyblock

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
  * @author Pratik Lodha
  * @brief Project for Heat Control of a seat
  * @version 0.1
- * @date 2021-04-24
+ * @date 2021-04-23
  *
  * @copyright Copyright (c) 2021
  *
@@ -13,17 +13,18 @@
 #include "Act_2.h"
 #include "Act_3.h"
 #include "Act_4.h"
-/**
- * @brief main function where code execution
+
+/** \brief main function where code execution
  *
- * @return int
+ * \param void
+ * \return int
+ *
  */
 int main(void)
 {
     while(1)
     {
-    Activity_1();
+    Activity_1(); // calling activity 1
     }
-
 return 0;
 }
