2c087f36f785b50a80a36a805069b22ce346b48b
prat7562 <81015406+prat7562@users.noreply.github.com>
2021-04-29 16:39:30 +0530

    Update Act_3.c

--- src/Act_3.c
+++ src/Act_3.c
@@ -1,7 +1,8 @@
+
 #include <avr/io.h>
 #include <util/delay.h>
-
-void Activity_3()
+int temperature=0;
+int Activity_3(void)
 {
 TCCR1A=(1<<COM1A1)|(1<<WGM11)|(WGM10);
 TCCR1B=(1<<WGM12)|(1<<CS11)|(1<<CS10);
@@ -12,23 +13,32 @@ PORTB |= (1<<PB1);
     {
         if(ADC <=200)
         {
+         temperature = 20;
+         Activity_4();
          OCR1A=102;
-        _delay_ms(200);
+         _delay_ms(200);
         }
         else if(ADC <= 500)
         {
-        OCR1A=204;
-        _delay_ms(200);
+         temperature = 25;
+         Activity_4();
+         OCR1A=204;
+         _delay_ms(200);
         }
         else if(ADC <= 700)
         {
-        OCR1A=358;
-        _delay_ms(200);
+         temperature = 29;
+         Activity_4();
+         OCR1A=358;
+         _delay_ms(200);
         }
         else if(ADC <= 1024)
         {
-        OCR1A=484;
-        _delay_ms(200);
+         temperature = 33;
+         Activity_4();
+         OCR1A=484;
+         _delay_ms(200);
         }
     }
+    return temperature;
 }
