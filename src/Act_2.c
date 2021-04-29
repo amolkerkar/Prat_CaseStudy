f9aa16b1418892920daad290f65ed1e8f8c85bac
prat7562 <81015406+prat7562@users.noreply.github.com>
2021-04-30 01:10:28 +0530

    added doxyblock

--- src/Act_2.c
+++ src/Act_2.c
@@ -1,12 +1,34 @@
-
+/**
+ * @file Act_2.c
+ * @author Pratik Lodha
+ * @brief Project for Heat Control of a seat
+ * @version 0.2 (Act-2)
+ * @date 2021-04-25
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include <avr/io.h>
 #include <util/delay.h>
 
+/** \brief InitADC function initializes the ADC
+ *
+ *
+ * \return void
+ *
+ */
 void InitADC()
 {
     ADMUX = (1<<REFS0);
     ADCSRA = (1<<ADEN)|(7<<ADPS0);
 }
+
+/** \brief ReadADC function reads the analog voltage at pin PC0, converts and stores binary bits in Register ADCL and ADCH
+ *
+ * \param ch uint8_t
+ * \return uint16_t
+ *
+ */
 uint16_t ReadADC(uint8_t ch)
 {
     ADMUX&=0xf8;
@@ -18,6 +40,11 @@ uint16_t ReadADC(uint8_t ch)
     return(ADC);
 }
 
+/** \brief main function of act-2, Reading the analog temperature and converting into digital using internal ADC
+ *
+ * \return void
+ *
+ */
 void Activity_2()
 {
     InitADC();
