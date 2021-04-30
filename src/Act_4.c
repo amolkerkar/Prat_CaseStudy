ed9f68eef5dc9049551910a87a953b39009be368
prat7562 <81015406+prat7562@users.noreply.github.com>
2021-04-30 16:59:00 +0530

    Update Act_4.c

--- src/Act_4.c
+++ src/Act_4.c
@@ -23,12 +23,11 @@ void USARTInit(uint16_t ubrr_value)
 {
     //set baud-rate
 
-    UBRR0L = ubrr_value;
-    UBRR0H = (ubrr_value>>8)&0x00ff;
-    UCSR0C =(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+    UBRR0L = ubrr_value;                                  // USART Baud Rate Register Low
+    UBRR0H = (ubrr_value>>8)&0x00ff;                      // USART Baud Rate Register High only 4 LSB bits
+    UCSR0C =(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);         // USART Control and Status Register in C // USART Mode Select(00 for Async) // Character Size
 
-    UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
-
+    UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0); // Receiver Enable // Transmitter Enable // RX and TX Complete Interrupt Enable
 }
 
 /** \brief This function writes char data in serial monitor for transmitting
@@ -39,11 +38,11 @@ void USARTInit(uint16_t ubrr_value)
  */
 char USARTWriteChar(char data)
 {
-    while(!(UCSR0A & (1<<UDRE0)))
+    while(!(UCSR0A & (1<<UDRE0)))           // USART Data Register Empty and ready to receive
     {
         //DO NOTHING
     }
-    UDR0 = data;
+    UDR0 = data;                            // USART I/O Data Register (16 bits)
     return 0;
 }
 
